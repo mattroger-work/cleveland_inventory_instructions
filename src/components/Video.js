0542eee7987a2a8357b829aca230df1aa0bc44d9
Matt Roger <matt.roger@teksynap.com>
2021-10-14 11:32:08 -0400

    mobile responsive

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,13 +5,17 @@ import './Video.css';
 
 function Video() {
   return (
-    <div className='video_container'>
-        <h1>Cleveland Inventory Instructions</h1>
-            <ReactPlayer className="video"
+    <>
+        <h1 className='videoText'>Cleveland Inventory Instructions</h1>
+        <div className='player-wrapper'>
+            <ReactPlayer className="react-player"
                 url={video}
                 controls={true}
+                width="100%"
+                height="100%"
             />
-    </div>
+          </div>
+    </>
   );
 }
 
